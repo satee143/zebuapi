0a4b486f312fab2625cb578c16619ff3a6d2485a
satee143 <satee143@gmail.com>
2020-07-20 22:49:59 +0530

    commited

---
+++ desk.py
@@ -0,0 +1,5 @@
+import psutil
+battery=psutil.sensors_battery()
+percent=battery.percent
+from pynotifier import Notification
+Notification(title='Battery Percentage',description=str(percent)+'% Battery Remaining',duration=5).send()
